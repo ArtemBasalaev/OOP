9cfa216ada70661ae36ee6554bb2ac29d98de964
ArtBas <art-basalaev@yandex.ru>
2021-07-16 19:48:32 +0700

    ver.2

--- Vector/src/ru/academits/basalaev/vector/Vector.java
+++ Vector/src/ru/academits/basalaev/vector/Vector.java
@@ -3,16 +3,14 @@ package ru.academits.basalaev.vector;
 import java.util.Arrays;
 
 public class Vector {
-    private int n;
-    private double[] array;
+    private double[] elements;
 
-    public Vector(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("Размерность вектора не может быть <=0, передано значение n = " + n);
+    public Vector(int elementsCount) {
+        if (elementsCount <= 0) {
+            throw new IllegalArgumentException("Размерность вектора не может быть <=0, передано значение n = " + elementsCount);
         }
 
-        this.n = n;
-        array = new double[n];
+        elements = new double[elementsCount];
     }
 
     public Vector(Vector vector) {
@@ -20,8 +18,7 @@ public class Vector {
             throw new IllegalArgumentException("Передана пустая ссылка, vector = null");
         }
 
-        array = copyArray(vector.array, vector.n);
-        n = vector.n;
+        elements = Arrays.copyOf(vector.elements, vector.elements.length);
     }
 
     public Vector(double[] array) {
@@ -33,67 +30,33 @@ public class Vector {
             throw new IllegalArgumentException("Передан массив нулевой длины");
         }
 
-        this.array = copyArray(array, array.length);
-        n = array.length;
+        elements = Arrays.copyOf(array, array.length);
     }
 
-    public Vector(int n, double[] array) {
+    public Vector(int elementsCount, double[] array) {
         if (array == null) {
             throw new IllegalArgumentException("Передана пустая ссылка, array = null");
         }
 
-        if (array.length == 0) {
-            throw new IllegalArgumentException("Передан массив нулевой длины");
+        if (elementsCount <= 0) {
+            throw new IllegalArgumentException("Размерность вектора не может быть <= 0, n = " + elementsCount);
         }
 
-        if (n <= 0) {
-            throw new IllegalArgumentException("Размерность вектора не может быть <=0, n = " + n);
-        }
-
-        if (n < array.length) {
-            throw new IllegalArgumentException("Размерность вектора меньше длинны массива: n = " + n +
-                    " длина массива length = " + array.length);
-        }
-
-        this.array = copyArray(array, n);
-        this.n = n;
-    }
-
-    public double[] getArray() {
-        return array;
-    }
-
-    public void setArray(double[] array) {
-        if (array == null) {
-            throw new IllegalArgumentException("Передана пустая ссылка, array = null");
-        }
-
-        if (array.length == 0) {
-            throw new IllegalArgumentException("Передан массив нулевой длины");
-        }
-
-        this.array = copyArray(array, array.length);
-        n = array.length;
+        elements = Arrays.copyOf(array, elementsCount);
     }
 
     public int getSize() {
-        return n;
-    }
-
-    private static double[] copyArray(double[] array, int length) {
-        double[] copy = new double[length];
-        int minLength = Math.min(array.length, length);
-
-        for (int i = 0; i < minLength; i++) {
-            copy[i] = array[i];
-        }
-
-        return copy;
+        return elements.length;
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(array);
+        StringBuilder sb = new StringBuilder(Arrays.toString(elements));
+
+        sb.setCharAt(0, '{');
+        sb.setCharAt(sb.length() - 1, '}');
+
+        return sb.toString();
     }
 
     @Override
@@ -108,7 +71,7 @@ public class Vector {
 
         Vector vector = (Vector) o;
 
-        return Arrays.equals(array, vector.array);
+        return Arrays.equals(elements, vector.elements);
     }
 
     @Override
@@ -116,139 +79,120 @@ public class Vector {
         final int prime = 37;
         int hash = 1;
 
-        hash = prime * hash + n;
-        hash = prime * hash + Arrays.hashCode(array);
+        hash = prime * hash + Arrays.hashCode(elements);
 
         return hash;
     }
 
-    public void getAddition(Vector vector) {
+    public void add(Vector vector) {
         if (vector == null) {
             throw new IllegalArgumentException("Передана пустая ссылка, vector = null");
         }
 
-        double[] result;
-        int minN = Math.min(n, vector.n);
-
-        if (n >= vector.n) {
-            result = array;
-        } else {
-            result = new double[vector.n];
+        if (elements.length < vector.elements.length) {
+            elements = Arrays.copyOf(elements, vector.elements.length);
         }
 
-        for (int i = 0; i < minN; i++) {
-            result[i] = array[i] + vector.array[i];
-        }
-
-        if (n < vector.n) {
-            for (int i = n; i < vector.n; i++) {
-                result[i] = vector.array[i];
-            }
-
-            array = result;
-            n = result.length;
+        for (int i = 0; i < vector.elements.length; i++) {
+            elements[i] += vector.elements[i];
         }
     }
 
-    public void getDifference(Vector vector) {
+    public void subtract(Vector vector) {
         if (vector == null) {
             throw new IllegalArgumentException("Передана пустая ссылка, vector = null");
         }
 
-        double[] result;
-        int minN = Math.min(n, vector.n);
-
-        if (n >= vector.n) {
-            result = array;
-        } else {
-            result = new double[vector.n];
+        if (elements.length < vector.elements.length) {
+            elements = Arrays.copyOf(elements, vector.elements.length);
         }
 
-        for (int i = 0; i < minN; i++) {
-            result[i] = array[i] - vector.array[i];
-        }
-
-        if (n < vector.n) {
-            for (int i = n; i < vector.n; i++) {
-                result[i] -= vector.array[i];
-            }
-
-            array = result;
-            n = result.length;
+        for (int i = 0; i < vector.elements.length; i++) {
+            elements[i] -= vector.elements[i];
         }
     }
 
-    public void getMultiplicationScalar(int scalar) {
-        for (int i = 0; i < n; i++) {
-            array[i] = array[i] * scalar;
+    public void multiplyScalar(double scalar) {
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] *= scalar;
         }
     }
 
-    public void getRevers() {
-        for (int i = 0; i < n; i++) {
-            array[i] *= -1;
-        }
+    public void reverse() {
+        multiplyScalar(-1);
     }
 
     public double getLength() {
         double sum = 0;
 
-        for (int i = 0; i < n; i++) {
-            sum += array[i] * array[i];
+        for (double element : elements) {
+            sum += element * element;
         }
 
         return Math.sqrt(sum);
     }
 
-    public double getValue(int index) {
-        if (index > n - 1 || index < 0) {
-            throw new IllegalArgumentException("Неверный индекс: index = " + index);
+    public double getElement(int index) {
+        if (index >= elements.length || index < 0) {
+            throw new IndexOutOfBoundsException("Неверный индекс: index = " + index);
         }
 
-        return array[index];
+        return elements[index];
     }
 
-    public void setValue(double value, int index) {
-        if (index > n - 1 || index < 0) {
-            throw new IllegalArgumentException("Неверный индекс: index = " + index);
+    public void setElement(int index, double value) {
+        if (index >= elements.length || index < 0) {
+            throw new IndexOutOfBoundsException("Неверный индекс: index = " + index);
         }
 
-        array[index] = value;
+        elements[index] = value;
     }
 
     public static Vector getSum(Vector vector1, Vector vector2) {
-        if (vector1 == null || vector2 == null) {
-            throw new IllegalArgumentException("Передана пустая ссылка");
+        if (vector1 == null) {
+            throw new IllegalArgumentException("В качестве первого аргумента передана пустая ссылка");
         }
 
-        Vector vector = new Vector(vector1);
-        vector.getAddition(vector2);
+        if (vector2 == null) {
+            throw new IllegalArgumentException("В качестве второго аргумента передана пустая ссылка");
+        }
 
-        return vector;
+        Vector result = new Vector(vector1);
+        result.add(vector2);
+
+        return result;
     }
 
-    public static Vector getDifference(Vector vector1, Vector vector2) {
-        if (vector1 == null || vector2 == null) {
-            throw new IllegalArgumentException("Передана пустая ссылка");
+    public static Vector getSubtract(Vector vector1, Vector vector2) {
+        if (vector1 == null) {
+            throw new IllegalArgumentException("В качестве первого аргумента передана пустая ссылка");
         }
 
-        Vector vector = new Vector(vector1);
-        vector.getDifference(vector2);
+        if (vector2 == null) {
+            throw new IllegalArgumentException("В качестве второго аргумента передана пустая ссылка");
+        }
 
-        return vector;
+        Vector result = new Vector(vector1);
+        result.subtract(vector2);
+
+        return result;
     }
 
     public static double getMultiplication(Vector vector1, Vector vector2) {
-        if (vector1 == null || vector2 == null) {
-            throw new IllegalArgumentException("Передана пустая ссылка");
+        if (vector1 == null) {
+            throw new IllegalArgumentException("В качестве первого аргумента передана пустая ссылка");
         }
 
-        int minN = Math.min(vector1.n, vector2.n);
+        if (vector2 == null) {
+            throw new IllegalArgumentException("В качестве второго аргумента передана пустая ссылка");
+        }
+
+        int minElementsCount = Math.min(vector1.elements.length, vector2.elements.length);
 
         double result = 0;
 
-        for (int i = 0; i < minN; i++) {
-            result += vector1.array[i] * vector2.array[i];
+        for (int i = 0; i < minElementsCount; i++) {
+            result += vector1.elements[i] * vector2.elements[i];
         }
 
         return result;
--- Vector/src/ru/academits/basalaev/vector_main/Main.java
+++ Vector/src/ru/academits/basalaev/vector_main/Main.java
@@ -13,26 +13,22 @@ public class Main {
         System.out.println(vector1);
         System.out.println(vector2);
 
-        vector1.getAddition(vector2);
+        vector1.add(vector2);
         System.out.println("Результат сложения вектора со вторым вектором:");
         System.out.println(vector1);
 
-        vector1.getMultiplicationScalar(5);
+        vector1.multiplyScalar(5);
         System.out.println("Результат умножения вектора на скаляр:");
         System.out.println(vector1);
 
-        vector1.getDifference(vector2);
+        vector1.subtract(vector2);
         System.out.println("Результат вычитания из вектора второго вектора:");
-        System.out.println(vector1);
-
-        vector1.setArray(new double[]{3, 2, 5});
-        System.out.println("Результат установки нового вектора:");
         System.out.println(vector1);
 
         System.out.printf("Длина вектора равна length = %.1f%n", vector1.getLength());
 
-        vector1.setValue(15, 2);
-        System.out.println("Результат установки компонента вектора по индексу:");
+        vector1.setElement(5, 2);
+        System.out.println("Результат установки компонента вектора по индексу 5:");
         System.out.println(vector1);
 
         double innerProduct = getMultiplication(vector1, vector2);
