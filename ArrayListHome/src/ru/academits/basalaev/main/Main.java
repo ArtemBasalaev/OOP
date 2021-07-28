4a9627cad508bb305eafa9853fc084dfaea37ad4
ArtBas <art-basalaev@yandex.ru>
2021-07-29 00:17:53 +0700

    Реализация задачи ArrayListHome

---
+++ ArrayListHome/src/ru/academits/basalaev/main/Main.java
@@ -0,0 +1,49 @@
+package ru.academits.basalaev.main;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> list1 = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new FileInputStream("input.txt"))) {
+            while (scanner.hasNextLine()) {
+                list1.add(scanner.nextLine());
+            }
+        }
+
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 5, 2, 2, 4, 7, 6, 5, 8));
+
+        int list2Size = list2.size();
+
+        for (int i = 0; i < list2Size; i++) {
+            if (list2.get(i) % 2 == 0) {
+                list2.remove(i);
+
+                list2Size--;
+                i--;
+            }
+        }
+
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(8, 5, 5, 5, 1, 4, 8, 3, 3, 3, 5, 2, 1, 4));
+
+        int list3Size = list3.size();
+
+        for (int i = 0; i < list3Size; i++) {
+            int listElement = list3.get(i);
+
+            for (int j = i + 1; j < list3Size; j++) {
+                if (list3.get(j) == listElement) {
+                    list3.remove(j);
+
+                    list3Size--;
+                    j--;
+                }
+            }
+        }
+    }
+}
