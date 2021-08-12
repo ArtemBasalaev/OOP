2e46eaa1e306231fb6f1140bcbcb148c8d303019
ArtBas <art-basalaev@yandex.ru>
2021-08-13 00:56:55 +0700

    изменена реализация метода getLinesListFromFile(String path)

--- ArrayListHome/src/ru/academits/basalaev/array_list_home_main/Main.java
+++ ArrayListHome/src/ru/academits/basalaev/array_list_home_main/Main.java
@@ -1,27 +1,25 @@
-package ru.academits.basalaev.ArrayListHome_main;
+package ru.academits.basalaev.array_list_home_main;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            ArrayList<String> stringListFromFile = getListFromFile("input.txt");
+            ArrayList<String> linesListFromFile = getLinesListFromFile("input.txt");
 
             System.out.println("Результат чтения файла в список");
-            System.out.println(stringListFromFile);
-        } catch (FileNotFoundException e) {
+            System.out.println(linesListFromFile);
+        } catch (IOException e) {
             System.out.println("Исходный файл не найден либо путь указан не верно");
         }
 
-        ArrayList<Integer> numberList = new ArrayList<>(Arrays.asList(2, 5, 2, 2, 4, 7, 6, 5, 8));
-        removeEvenNumbers(numberList);
+        ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(2, 5, 2, 2, 4, 7, 6, 5, 8));
+        removeEvenNumbers(numbersList);
 
         System.out.println("Список после удаления четных чисел:");
-        System.out.println(numberList);
+        System.out.println(numbersList);
 
         ArrayList<Integer> numbersListWithDuplicates =
                 new ArrayList<>(Arrays.asList(8, 5, 5, 7, 255, 1, 4, 8, 8, 3, 3, 3, 5, 255, 1, 4, 2, 2, 7, 7));
@@ -30,12 +28,15 @@ public class Main {
         System.out.println(getListWithoutDuplicates(numbersListWithDuplicates));
     }
 
-    public static ArrayList<String> getListFromFile(String path) throws FileNotFoundException {
+    public static ArrayList<String> getLinesListFromFile(String path) throws IOException {
         ArrayList<String> list = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner(new FileInputStream(path))) {
-            while (scanner.hasNextLine()) {
-                list.add(scanner.nextLine());
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            String string = in.readLine();
+
+            while (string != null) {
+                list.add(string);
+                string = in.readLine();
             }
         }
 
@@ -51,11 +52,11 @@ public class Main {
     }
 
     public static ArrayList<Integer> getListWithoutDuplicates(ArrayList<Integer> list) {
-        ArrayList<Integer> listWithoutDuplicates = new ArrayList<>();
+        ArrayList<Integer> listWithoutDuplicates = new ArrayList<>(list.size());
 
-        for (Integer listElement : list) {
-            if (!listWithoutDuplicates.contains(listElement)) {
-                listWithoutDuplicates.add(listElement);
+        for (Integer e : list) {
+            if (!listWithoutDuplicates.contains(e)) {
+                listWithoutDuplicates.add(e);
             }
         }
 
